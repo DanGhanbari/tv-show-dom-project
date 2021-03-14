05efcab9b766e31f6bf17df8752a3d9c255a88a3
Dan <danielghanbari7@gmail.com>
2021-03-14 23:19:24 +0000

    Level 100

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-//You can edit ALL of the code here
 function setup() {
   const allEpisodes = getAllEpisodes();
   makePageForEpisodes(allEpisodes);
@@ -6,7 +5,39 @@ function setup() {
 
 function makePageForEpisodes(episodeList) {
   const rootElem = document.getElementById("root");
-  rootElem.textContent = `Got ${episodeList.length} episode(s)`;
+  const mainElement = document.createElement("main");
+
+  for (let i = 0; i < episodeList.length; i++) {
+    //making elements and adding class
+    let sectionElement = document.createElement("section");
+    let aTag = document.createElement("a");
+    let h1Element = document.createElement("h1");
+    let imageElement = document.createElement("img");
+    let pElement = document.createElement("p");
+    pElement.className = "description";
+
+    // adding zero to numbers less than 10
+    if (episodeList[i].number < 10) {
+      episodeList[i].number = `0${episodeList[i].number}`;
+    }
+    if (episodeList[i].season < 10) {
+      episodeList[i].season = `0${episodeList[i].season}`;
+    }
+    //adding value to elements
+    aTag.href = `${episodeList[i]._links.self.href}`;
+    aTag.innerText = `${episodeList[i].name} - S${episodeList[i].season}E${episodeList[i].number}`;
+    aTag.target = "_blank";
+    imageElement.src = `${episodeList[i].image.medium}`;
+    pElement.innerHTML = `${episodeList[i].summary}`;
+
+    //appending elements
+    h1Element.appendChild(aTag);
+    sectionElement.appendChild(h1Element);
+    sectionElement.appendChild(imageElement);
+    sectionElement.appendChild(pElement);
+    mainElement.appendChild(sectionElement);
+    rootElem.appendChild(mainElement);
+  }
 }
 
 window.onload = setup;
