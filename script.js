6eb03b977b6ad10dda7830c5c8f29125df2f395b
Dan <danielghanbari7@gmail.com>
2021-03-26 12:30:15 +0000

    Level 400

--- script.js
+++ script.js
@@ -1,38 +1,35 @@
+"use strict";
 const rootElem = document.getElementById("root");
 const mainElement = document.createElement("main");
+const optionElement = document.createElement("option");
+const selectEpisodeElement = document.getElementById("episode-select");
+const selectShowElement = document.getElementById("show-select");
+const inputElement = document.querySelector("input");
 
 function setup() {
-  fetch("https://api.tvmaze.com/shows/82/episodes")
-    .then((response) => response.json())
-    .then((data) => {
-      const allEpisodes = data;
-      makePageForEpisodes(allEpisodes);
-      searchBox();
-      dropDown(allEpisodes);
-    })
-    .catch((err) => console.log("Error"));
+  selectShow();
+}
+
+function addZero(x) {
+  if (x < 10) {
+    return (x = `0${x}`);
+  } else {
+    return x;
+  }
 }
 
 function makePageForEpisodes(episodeList) {
   for (let i = 0; i < episodeList.length; i++) {
-    //making elements and adding class
     let sectionElement = document.createElement("section");
     let aTag = document.createElement("a");
-    let h1Element = document.createElement("h1");
+    let h1Element = document.createElement("h3");
     let imageElement = document.createElement("img");
     let pElement = document.createElement("p");
-
-    // adding zero to numbers less than 10
-    if (episodeList[i].number < 10) {
-      episodeList[i].number = `0${episodeList[i].number}`;
-    }
-    if (episodeList[i].season < 10) {
-      episodeList[i].season = `0${episodeList[i].season}`;
-    }
-    //adding value to elements
+    let seasonNumber = addZero(episodeList[i].season);
+    let episodeNumber = addZero(episodeList[i].number);
     aTag.href = `${episodeList[i].url}`;
     aTag.target = "_blank";
-    h1Element.innerText = `${episodeList[i].name} - S${episodeList[i].season}E${episodeList[i].number}`;
+    h1Element.innerText = `${episodeList[i].name} - S${seasonNumber}E${episodeNumber}`;
     imageElement.src = `${episodeList[i].image.medium}`;
     pElement.innerHTML = `${episodeList[i].summary}`;
 
@@ -48,7 +45,6 @@ function makePageForEpisodes(episodeList) {
 
 //search box
 function searchBox() {
-  const inputElement = document.querySelector("input");
   inputElement.addEventListener("keyup", function (event) {
     let inputLetter = event.target.value.toLowerCase();
     let searchedEpisode = document.getElementById("search-result");
@@ -73,27 +69,82 @@ function searchBox() {
 
 // Drop-down option menu
 function dropDown(episodes) {
-  const selectElement = document.getElementById("episode-select");
+  let firstOption = document.createElement("option");
+  firstOption.innerHTML = "Select all episodes";
+  selectEpisodeElement.appendChild(firstOption);
   for (let i = 0; i < episodes.length; i++) {
-    var optionElement = document.createElement("option");
+    let optionElement = document.createElement("option");
     optionElement.value = `<section>
-                            <a href = ${episodes[i].url}><h1>S${episodes[i].season}E${episodes[i].number} - ${episodes[i].name}</h1></a>
-                            <img src = ${episodes[i].image.medium}>
-                            <p>${episodes[i].summary}</p>
-                          </section>`;
+    <a href = ${episodes[i].url}><h3>S${episodes[i].season}E${episodes[i].number} - ${episodes[i].name}</h3></a>
+    <img src = ${episodes[i].image.medium}>
+    <p>${episodes[i].summary}</p>
+    </section>`;
 
-    optionElement.innerHTML = `S${episodes[i].season}E${episodes[i].number} - ${episodes[i].name}`;
-    selectElement.appendChild(optionElement);
+    let seasonNumber = addZero(episodes[i].season);
+    let episodeNumber = addZero(episodes[i].number);
+    optionElement.innerHTML = `S${seasonNumber}E${episodeNumber} - ${episodes[i].name}`;
+    selectEpisodeElement.appendChild(optionElement);
   }
-  selectElement.addEventListener("change", () => {
-    if (selectElement.selectedIndex === 0) {
-      mainElement.innerHTML = "";
-      window.onload();
+  selectEpisodeElement.addEventListener("change", () => {
+    if (selectEpisodeElement.selectedIndex === 0) {
+      selectedShow();
     } else {
-      mainElement.innerHTML = selectElement.value;
+      mainElement.innerHTML = "";
+      mainElement.innerHTML = selectEpisodeElement.value;
       rootElem.appendChild(mainElement);
     }
   });
+}
+
+//select show
+let selectShow = function () {
+  const showList = getAllShows();
+  var showArr = [];
+  var idArr = [];
+  for (let i = 0; i < showList.length; i++) {
+    showArr.push(showList[i].name);
+    idArr.push(showList[i].id);
+  }
+
+  var zip = [];
+  for (var i = 0; i < showArr.length; i++) {
+    zip.push([showArr[i], idArr[i]]);
+  }
+
+  zip.sort();
+
+  for (var i = 0; i < zip.length; i++) {
+    showArr[i] = zip[i][0];
+    idArr[i] = zip[i][1];
+  }
+  for (let i = 0; i < showArr.length; i++) {
+    let optionElement = document.createElement("option");
+    optionElement.value = `${idArr[i]}`;
+    optionElement.innerHTML = `${showArr[i]}`;
+    selectShowElement.appendChild(optionElement);
+  }
+};
+
+//adding event listener to choose selected show
+selectShowElement.addEventListener("change", selectedShow);
+
+function selectedShow() {
+  if (selectShowElement.selectedIndex === 0) {
+    mainElement.innerHTML = "";
+  } else {
+    mainElement.innerHTML = "";
+    let showId = selectShowElement.value;
+    selectEpisodeElement.innerHTML = "";
+    fetch(`https://api.tvmaze.com/shows/${showId}/episodes`)
+      .then((response) => response.json())
+      .then((data) => {
+        const allEpisodes = data;
+        dropDown(allEpisodes);
+        makePageForEpisodes(allEpisodes);
+        searchBox(allEpisodes);
+      })
+      .catch((Error) => console.log("Error"));
+  }
 }
 
 window.onload = setup;
